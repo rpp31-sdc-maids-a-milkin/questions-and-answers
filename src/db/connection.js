91e846e1037515644bf099f4673076d1a93ba8e6
jc <jake.caughern@gmail.com>
2022-01-15 13:47:27 -0600

    changed timeout

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/qa';
 console.log('mongo uri -----> ', uri);
 
-const conn = mongoose.connect(uri);
+const conn = mongoose.connect(uri, { connectTimeoutMS: 20000 });
 
 mongoose.connection.on('error', (err) => console.error(err));
 
