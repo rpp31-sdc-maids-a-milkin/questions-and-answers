f3427cdf1c706063ce4aa792c616865a91d7df75
jc <jake.caughern@gmail.com>
2022-01-15 11:19:53 -0600

    fixed merge issues

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,7 @@
 /* eslint-disable camelcase */
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-const uri = process.env.MONGO_URI || 'mongodb://34.201.70.69:27017/qa';
-=======
 const uri = process.env.MONGO_URL || 'mongodb://localhost:27017/qa';
->>>>>>> main
 
 const conn = mongoose.connect(uri);
 
